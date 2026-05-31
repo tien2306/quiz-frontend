a30c30d3f5f3cabda98b5336e59b3ede03dafbcf
thanh tien <thanhtien23062005@gmail.com>
2026-05-31 16:33:05 +0700

    fix login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { useDispatch } from "react-redux";
 import "./App.css";
+
 import AllRoutes from "./components/Allroute";
+import { getCookie } from "./helpers/cookie";
+import { checkLogin } from "./actions/login";
+import { useEffect } from "react";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const token = getCookie("token");
+    if (token) {
+      dispatch(checkLogin(true));
+    }
+  }, [dispatch]);
+
   return (
     <>
       <AllRoutes />
--- src/components/PrivateRoutes/index.js
+++ src/components/PrivateRoutes/index.js
@@ -1,20 +1,9 @@
-import { useDispatch, useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
 import Error404 from "../../pages/Error404";
-import { useEffect } from "react";
-import { checkLogin } from "../../actions/login";
-import { getCookie } from "../../helpers/cookie";
+import { useSelector } from "react-redux";
 
 function PrivateRoutes() {
   const state = useSelector((state) => state.loginReducer);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    const token = getCookie("token");
-    if (token) {
-      dispatch(checkLogin(true));
-    }
-  }, [dispatch]);
 
   return <>{state ? <Outlet /> : <Error404 />}</>;
 }
